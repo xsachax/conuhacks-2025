30c2bb2d4920abf9c262b30bfe8526cdfff923ad
Carson <92652800+carsonSgit@users.noreply.github.com>
2025-02-02 02:24:59 -0500

    tempfix: Onclick to play audio

--- src/App.tsx
+++ src/App.tsx
@@ -9,42 +9,42 @@ import Character from "./Character";
 import Adventurer from "./Adventurer";
 import { useConvoStore } from "./utils/convoHelper";
 import Convo from "./ui/Convo";
-import global from "./assets/sfx/global.mp3"
+import global from "./assets/sfx/global.mp3";
 
 export default function App() {
   //const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   const { convoActive } = useConvoStore();
+  const [audioPlayed, setAudioPlayed] = useState(false);
+
+  const handleUserInteraction = () => {
+    if (!audioPlayed) {
+      const audio = new Audio(global);
+      audio.volume = 0.5;
+      audio.loop = true;
+      audio.play().catch((error) => {
+        console.error('Audio play error:', error);
+      });
+      setAudioPlayed(true);
+    }
+  };
 
   useEffect(() => {
-    const audio = new Audio(global);
-    audio.volume = 0.5;
-    audio.loop = true;
-    audio.play();
+    window.addEventListener("click", handleUserInteraction);
 
     return () => {
-      audio.pause();
-      audio.currentTime = 0;
+      window.removeEventListener("click", handleUserInteraction);
     };
-  }, []);
+  }, [audioPlayed]);
 
   return (
     <>
       {convoActive && <Convo />}
-      <Canvas shadows camera={{ position: [0, 5, 120], fov: 80}}>
-
+      <Canvas shadows camera={{ position: [0, 5, 120], fov: 80 }}>
         <Perf position="top-left" />
-        <Physics
-          timeStep="vary"
-          // debug
-        >
+        <Physics timeStep="vary">
           <Lights />
-          <Sky
-            distance={1000} 
-            sunPosition={[-100, -1, -10]}
-            inclination={0.5}
-            azimuth={0.25}
-          />
+          <Sky distance={1000} sunPosition={[-100, -1, -10]} inclination={0.5} azimuth={0.25} />
           <Suspense fallback={null}>
             <Adventurer />
             <World />
