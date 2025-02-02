a793b970ecd14ab47bf4f480b417ea2de73e8402
Carson <92652800+carsonSgit@users.noreply.github.com>
2025-02-01 20:59:50 -0500

    All Characters placed

--- src/World.tsx
+++ src/World.tsx
@@ -1,5 +1,9 @@
 import { RigidBody } from "@react-three/rapier";
 import Horse from "./models/Horse";
+import Steve from "./models/Steve";
+import Bender from "./models/Bender";
+import Wario from "./models/Wario";
+import Peter from "./models/Peter";
 import Island from "./models/Island";
 import TranslationHelper from "./TranslationHelper";
 
@@ -14,6 +18,10 @@ export default function World() {
       </group>
       <TranslationHelper position={[0, 0, 0]} />
       <Horse position={[-22, -12.7, 10]} rotation={[0, 4, 0]} scale={2.5} dialog={{}} />
+      <Steve position={[-22, -13.1, 0]} rotation={[0, 4, 0]} scale={2.5} dialog={{}} />
+      <Bender position={[-22, -13.3, -10]} rotation={[0, 5, 0]} scale={2.5} dialog={{}} />
+      <Wario position={[-57, -15.2, -10]} rotation={[0, -4.3, 0]} scale={2.5} dialog={{}} />
+      <Peter position={[-55, -13.3, -2]} rotation={[0, -4.5, 0]} scale={150} dialog={{}} />
     </>
   );
 }
